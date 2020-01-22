707ad0c5668b54f99086ba04edbe913556609a8f
MaraMarp <50228905+MaraMarp@users.noreply.github.com>
2020-01-22 08:14:46 +0100

    Initial commit

---
+++ WaterDetector/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WaterDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            WaterTank wt1 = new WaterTank(100, "WT1");
+            WaterTank wt2 = new WaterTank(10, "WT2");
+            WaterTank wt3 = new WaterTank(500, "WT3");
+            WaterTank wt4 = new WaterTank(50, "WT4");
+            WaterTank wt5 = new WaterTank(25, "WT5");
+            while (true)
+            {
+                wt1.Add(r.Next(5));
+                wt2.Add(r.Next(5));
+                wt3.Add(r.Next(5));
+                wt4.Add(r.Next(5));
+                wt5.Add(r.Next(5));
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ WaterDetector/WaterTank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterDetector
+{
+    class WaterTank
+    {
+        private int _value = 0;
+
+        public WaterTank(int capacity, string name)
+        {
+            Capacity = capacity;
+            Name = name;
+            Reset();
+        }
+
+        public int Capacity { get; }
+        public string Name { get; }
+        public int CurrentValue { get { return _value; } }
+        public void Reset()
+        {
+            _value = 0;
+        }
+
+        public void Add(int value)
+        {
+            _value += value;
+        }
+    }
+}
